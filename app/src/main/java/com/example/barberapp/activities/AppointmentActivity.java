b1abdf34950603a5a1c6abafedc2b2215b508c7d
yarin <you@example.com>
2021-05-05 15:59:12 +0300

    v0.05 added select treatment logic

--- app/src/main/java/com/example/barberapp/activities/AppointmentActivity.java
+++ app/src/main/java/com/example/barberapp/activities/AppointmentActivity.java
@@ -1,25 +1,32 @@
 package com.example.barberapp.activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.barberapp.R;
 import com.example.barberapp.databinding.ActivityAppointmentBinding;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class AppointmentActivity extends AppCompatActivity {
 
 
     private ActivityAppointmentBinding binding;
+    private boolean[] selectTreatment;
+    private ArrayList<Integer> treatmentsList = new ArrayList<>();
+    private String[] treatmentsArray = {"Blow Dry","Men's Hair Cut","Beard Trim",};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_appointment);
+
         initViews();
     }
 
@@ -27,6 +34,35 @@ public class AppointmentActivity extends AppCompatActivity {
         binding.appointmentSearchButton.setOnClickListener(v -> {
             Intent intent = new Intent(this, TimeStampActivity.class);
             startActivity(intent);
+        });
+        selectTreatment = new boolean[treatmentsArray.length];
+        binding.appointmentSelectBarber.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(
+                    AppointmentActivity.this
+            );
+            builder.setTitle("Select Barber");
+            builder.setCancelable(false);
+            builder.setMultiChoiceItems(treatmentsArray, selectTreatment,
+                    (dialog, which, isChecked) -> {
+                        if (isChecked) {
+                            treatmentsList.add(which);
+                            Collections.sort(treatmentsList);
+
+                        } else {
+                            treatmentsList.remove(which);
+                        }
+                    });
+
+
+            builder.setPositiveButton("OK", (dialog, which) -> {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int j = 0; j < treatmentsList.size(); j++) {
+                    stringBuilder.append(treatmentsArray[treatmentsList.get(j)]);
+                    if (j != treatmentsList.size() - 1) {
+
+                    }
+                }
+            });
         });
     }
 
