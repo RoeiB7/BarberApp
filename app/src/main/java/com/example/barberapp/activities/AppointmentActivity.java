0d579f1fec93034ea8bb1b8a31bac8354719e3a6
roeibarkan <barkan.dev@gmail.com>
2021-05-06 12:23:59 +0300

    v0.06 added logic for select treatment

--- app/src/main/java/com/example/barberapp/activities/AppointmentActivity.java
+++ app/src/main/java/com/example/barberapp/activities/AppointmentActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.example.barberapp.R;
 import com.example.barberapp.databinding.ActivityAppointmentBinding;
@@ -19,7 +23,8 @@ public class AppointmentActivity extends AppCompatActivity {
     private ActivityAppointmentBinding binding;
     private boolean[] selectTreatment;
     private ArrayList<Integer> treatmentsList = new ArrayList<>();
-    private String[] treatmentsArray = {"Blow Dry","Men's Hair Cut","Beard Trim",};
+    private String[] treatmentsArray = {"Blow Dry", "Men's Hair Cut", "Beard Trim", "another one"};
+    //private Integer[]
 
 
     @Override
@@ -36,36 +41,69 @@ public class AppointmentActivity extends AppCompatActivity {
             startActivity(intent);
         });
         selectTreatment = new boolean[treatmentsArray.length];
-        binding.appointmentSelectBarber.setOnClickListener(v -> {
-            AlertDialog.Builder builder = new AlertDialog.Builder(
-                    AppointmentActivity.this
-            );
-            builder.setTitle("Select Barber");
-            builder.setCancelable(false);
-            builder.setMultiChoiceItems(treatmentsArray, selectTreatment,
-                    (dialog, which, isChecked) -> {
-                        if (isChecked) {
-                            treatmentsList.add(which);
-                            Collections.sort(treatmentsList);
-
-                        } else {
-                            treatmentsList.remove(which);
-                        }
-                    });
-
-
-            builder.setPositiveButton("OK", (dialog, which) -> {
-                StringBuilder stringBuilder = new StringBuilder();
-                for (int j = 0; j < treatmentsList.size(); j++) {
-                    stringBuilder.append(treatmentsArray[treatmentsList.get(j)]);
-                    if (j != treatmentsList.size() - 1) {
-
-                    }
-                }
-            });
+        binding.appointmentSelectTreatment.setOnClickListener(v -> {
+            buildMultiSelect();
         });
+
+
     }
 
-    //todo: add count number for amout of time of treatment in design(TextView)
+    private void buildMultiSelect() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(
+                AppointmentActivity.this
+        );
+        builder.setTitle("Select Treatment - Up to 3 ");
+        builder.setCancelable(false);
+
+        builder.setMultiChoiceItems(treatmentsArray, selectTreatment, new DialogInterface.OnMultiChoiceClickListener() {
+            int count = 0;
+
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                if (isChecked) {
+                    if (!treatmentsList.contains(which))
+                        if (treatmentsList.size() < 3) {
+                            treatmentsList.add(which);
+                            selectTreatment[which] = true;
+                        } else {
+                            count--;
+                            ((AlertDialog) dialog).getListView().setItemChecked(which, false);
+                            selectTreatment[which] = false;
+                            Toast.makeText(getApplicationContext(), "you can't add more", Toast.LENGTH_SHORT).show();
+                        }
+                } else if (treatmentsList.contains(which)) {
+                    treatmentsList.remove(which);
+                    selectTreatment[which] = false;
+                }
+            }
+        });
+
+        builder.setPositiveButton("OK", (dialog, which) ->
+                writeTreatment());
+        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
+
+        builder.setNeutralButton("Clear All", (dialog, which) ->
+
+        {
+            for (int i = 0; i < selectTreatment.length; i++) {
+                selectTreatment[i] = false;
+                treatmentsList.clear();
+                binding.appointmentSelectTreatment.setText("");
+            }
+        });
+        builder.show();
+    }
+
+    private void writeTreatment() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int j = 0; j < treatmentsList.size(); j++) {
+            stringBuilder.append(treatmentsArray[treatmentsList.get(j)]);
+            if (j != treatmentsList.size() - 1) {
+                stringBuilder.append(", ");
+            }
+        }
+        binding.appointmentSelectTreatment.setText(stringBuilder.toString());
+    }
+
 
 }
